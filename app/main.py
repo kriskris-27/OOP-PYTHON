f1b3cd3ebb49e932e404e27229aa0c5c61a65763
KrishnakumarS <krishnakumars0101@gmail.com>
2026-06-07 00:01:24 +0530

    restructured main app

--- app/main.py
+++ app/main.py
@@ -1,15 +1,9 @@
-from fastapi import FastAPI
-
-from app.routers import health
-
-app = FastAPI(
-    title="My API",
-    version="0.1.0",
-)
-
-app.include_router(health.router)
+from fastapi import APIRouter
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI"}
+router = APIRouter() # Router object
+
+@router.get("/health")
+
+def health_check():
+    return {"status":"healthy"}
