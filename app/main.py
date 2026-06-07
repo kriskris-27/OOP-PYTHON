326c917de1f83ae59ff08efabaa63d8ed32f0e08
KrishnakumarS <krishnakumars0101@gmail.com>
2026-06-07 23:59:29 +0530

    added a object

--- app/main.py
+++ app/main.py
@@ -7,3 +7,10 @@ router = APIRouter() # Router object
 
 def health_check():
     return {"status":"healthy"}
+
+def get_user_service():
+    return UserService()  # Returns an object
+
+@router.get("/users")
+def get_users(service: UserService = Depends(get_user_service)):
+    return service.get_all_users()
