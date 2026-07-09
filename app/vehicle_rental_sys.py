e8dfb3561a1bdd3ebf126c9c8f4a25ce4e1a79f3
KrishnakumarS <krishnakumars0101@gmail.com>
2026-07-10 01:59:50 +0530

    Task 3: Vehicle Rental System completed

--- app/vehicle_rental_sys.py
+++ app/vehicle_rental_sys.py
@@ -1,5 +1,5 @@
 class Vehicle:
-    def __init__(self,make,model,year,daily_rate,is_rented):
+    def __init__(self,make,model,year,daily_rate):
         self.make = make
         self.model = model
         self.year = year
@@ -28,7 +28,7 @@ class Vehicle:
         if self.is_rented:
             raise ValueError(f"{self.make} {self.model} is already out for rent")
         self.is_rented = True
-        print(f"{self.make} {self.model}Checked out succesfully.")
+        print(f"{self.make} {self.model} Checked out succesfully.")
         
 
 
@@ -46,8 +46,8 @@ class Vehicle:
 
 
 class Car(Vehicle):
-    def __init__(self, make, model, year, daily_rate, is_rented,num_doors,transmission_type):
-        super().__init__(make, model, year, daily_rate, is_rented)
+    def __init__(self, make, model, year, daily_rate,num_doors,transmission_type):
+        super().__init__(make, model, year, daily_rate)
 
         self.num_doors = num_doors
         self.transmission_type = transmission_type
@@ -66,9 +66,39 @@ class Truck(Vehicle):
         self.cargo_capacity = cargo_capacity
         self.requires_license = requires_license
 
-    
+    #method override
     def calculate_rental_cost(self, days):
-        base_cost =  super().calculate_rental_cost(days)
+        base_cost =  super().calculate_rental_cost(days) #including those existing code and adding some additional changes particularly works for this interms of buisness logic
         cargo_surcharge = self.cargo_capacity * 500
 
-        return base_cost + cargo_surcharge
+        return base_cost + cargo_surcharge 
+
+  
+def process_rental_fleet(vehicle_fleet , rental_days):
+
+    total_revenue = 0
+
+    for vehicle in vehicle_fleet:
+
+        try:
+            vehicle.rent()
+            cost = vehicle.calculate_rental_cost(rental_days)
+
+            total_revenue+=cost
+        except ValueError as err:
+            print(f" -> [TRANSACTION DENIED]: {err}")
+            
+        print("-" * 65)
+
+    print(f"TOTAL SYSTEM BATCH REVENUE : ₹{total_revenue:,.2f}\n")
+   
+
+        
+if __name__ == "__main__":
+    car1 = Car("Maruti", "Swift", 2023, 1200.0, num_doors=4, transmission_type="Manual")
+    bike1 = Motorcycle("KTM", "Adventure 390", 2024, 1500.0, engine_cc=373, has_helmet=True)
+    truck1 = Truck("BharatBenz", "1617R", 2022, 5000.0, cargo_capacity=7.5, requires_license=True)
+
+    my_fleet = [car1,bike1,truck1]
+
+    process_rental_fleet(my_fleet,rental_days=5)
